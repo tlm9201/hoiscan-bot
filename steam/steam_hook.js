ee5960b3a068d96c28f289f39f7e1aa7a331c962
ewof <marroq@marroq.xyz>
2024-12-23 17:27:24 -0500

    nerd ass inline slop (the file is less lines wholesome)

--- steam/steam_hook.js
+++ steam/steam_hook.js
@@ -6,30 +6,8 @@ class Hoi {
     }
 
     async search(query) {
-        const foundLobbies = []
-        const lobbies = await this.client.matchmaking.getLobbies() 
-        
-        if (query === undefined) {
-            lobbies.forEach((lobby) => {
-                const data = lobby.getFullData()
-                foundLobbies.push(data) // add
-            })
-        } else {
-            lobbies.forEach((lobby) => {
-                const data = lobby.getFullData()
-    
-                // check if this lobby matches the search
-                if (dataMatchesSearch(query, data))
-                    foundLobbies.push(data) // add
-            })
-        }
-
-        return foundLobbies 
+        return (await this.client.matchmaking.getLobbies() ).map((lobby) => lobby.getFullData()).filter((lobby) => lobby.password === '0' && (query === undefined || lobby.name.toLowerCase().includes(query.toLowerCase())))
     }
-}
-
-function dataMatchesSearch(query, data) {
-    return data.name.toLowerCase().includes(query.toLowerCase())
 }
 
 const hoi = new Hoi()
